f4036f298faf7672c50dcb1f4f9bf4e8e70374b4
groovy-gorilla <arturlewandowski79@gmail.com>
2026-06-10 17:50:44 +0200

    ScenePass migrated to Dynamic Rendering

--- Engine/Graphics/Vulkan/ScenePass/ScenePass.cpp
+++ Engine/Graphics/Vulkan/ScenePass/ScenePass.cpp
@@ -5,126 +5,14 @@
 
 void ScenePass::Create(VkDevice device, VkPhysicalDevice physicalDevice, VkExtent2D renderExtent, VkFormat colorFormat, VkFormat depthFormat, AntiAliasing aaMode, VkSampleCountFlagBits samples, Settings& settings) {
 
-  m_renderExtent = renderExtent;
-
-    // COLOR MSAA
-    VkAttachmentDescription msaa{};
-    msaa.format = colorFormat;
-    msaa.samples = samples;
-    msaa.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
-    msaa.storeOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-    msaa.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
-    msaa.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-    msaa.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-    msaa.finalLayout   = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
-
-    // DEPTH MSAA
-    VkAttachmentDescription depth{};
-    depth.format = depthFormat;
-    depth.samples = samples;
-    depth.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
-    depth.storeOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-    depth.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
-    depth.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-    depth.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-    depth.finalLayout   = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
-
-    // RESOLVE
-    VkAttachmentDescription resolve{};
-    resolve.format = colorFormat;
-    resolve.samples = VK_SAMPLE_COUNT_1_BIT;
-    resolve.loadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
-    resolve.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
-    resolve.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
-    resolve.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-    resolve.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-    resolve.finalLayout   = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
-
-    // ATTACHMENTS
-    std::vector<VkAttachmentDescription> attachments;
-    attachments.push_back(msaa);            // 0
-    attachments.push_back(depth);           // 1
-    attachments.push_back(resolve);         // 2
-
-    VkAttachmentReference colorRef{};
-    colorRef.attachment = 0;
-    colorRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
-
-    VkAttachmentReference depthRef{};
-    depthRef.attachment = 1;
-    depthRef.layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
-
-    VkAttachmentReference resolveRef{};
-    resolveRef.attachment = 2;
-    resolveRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
-
-    // SUBPASS
-    VkSubpassDescription subpass{};
-    subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
-    subpass.colorAttachmentCount = 1;
-    subpass.pColorAttachments = &colorRef;
-    subpass.pDepthStencilAttachment = &depthRef;
-
-    if (aaMode == AntiAliasing::MSAA || aaMode == AntiAliasing::MSAA_SMAA) {
-        subpass.pResolveAttachments = &resolveRef;
-    } else {
-        subpass.pResolveAttachments = nullptr;
-    }
-
-    // DEPENDENCIES
-    VkSubpassDependency dep1{};
-    dep1.srcSubpass = VK_SUBPASS_EXTERNAL;
-    dep1.dstSubpass = 0;
-    dep1.srcStageMask = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
-    dep1.srcAccessMask = VK_ACCESS_SHADER_READ_BIT;
-    dep1.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT | VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;;
-    dep1.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;;
-
-    VkSubpassDependency dep2{};
-    dep2.srcSubpass = 0;
-    dep2.dstSubpass = VK_SUBPASS_EXTERNAL;
-    dep2.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT | VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT;;
-    dep2.srcAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;;
-    dep2.dstStageMask = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
-    dep2.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
-
-    VkSubpassDependency dependencies[] = { dep1, dep2 };
-
-    // CREATE RENDER PASS
-    VkRenderPassCreateInfo createInfo{};
-    createInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
-    createInfo.attachmentCount = static_cast<uint32_t>(attachments.size());
-    createInfo.pAttachments = attachments.data();
-    createInfo.subpassCount = 1;
-    createInfo.pSubpasses = &subpass;
-    createInfo.dependencyCount = 2;
-    createInfo.pDependencies = dependencies;
-
-    VK_CHECK(vkCreateRenderPass(device, &createInfo, nullptr, &m_renderPass));
+    m_renderExtent = renderExtent;
 
     // CREATE RENDER TARGETS
     m_color.Create(device, physicalDevice, m_renderExtent.width, m_renderExtent.height, colorFormat, VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_SAMPLED_BIT, VK_IMAGE_ASPECT_COLOR_BIT, samples);
     m_depth.Create(device, physicalDevice, m_renderExtent.width, m_renderExtent.height, depthFormat, VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT, VK_IMAGE_ASPECT_DEPTH_BIT, samples);
     m_resolve.Create(device, physicalDevice, m_renderExtent.width, m_renderExtent.height, colorFormat, VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_SAMPLED_BIT, VK_IMAGE_ASPECT_COLOR_BIT, VK_SAMPLE_COUNT_1_BIT);
 
-    // FRAMEBUFFER
-    std::vector<VkImageView> imageAttachments = {
-        m_color.GetImageView(),
-        m_depth.GetImageView(),
-        m_resolve.GetImageView()
-    };
-
-    VkFramebufferCreateInfo framebufferCreateInfo{};
-    framebufferCreateInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
-    framebufferCreateInfo.renderPass = m_renderPass;
-    framebufferCreateInfo.attachmentCount = static_cast<uint32_t>(imageAttachments.size());
-    framebufferCreateInfo.pAttachments = imageAttachments.data();
-    framebufferCreateInfo.width = m_renderExtent.width;
-    framebufferCreateInfo.height = m_renderExtent.height;
-    framebufferCreateInfo.layers = 1;
-
-    VK_CHECK(vkCreateFramebuffer(device, &framebufferCreateInfo, nullptr, &m_framebuffer));
-
+    // PIPELINE
     PipelineDesc pdesc;
     pdesc.renderPass = VK_NULL_HANDLE;
     pdesc.colorFormat = colorFormat;
@@ -135,7 +23,6 @@ void ScenePass::Create(VkDevice device, VkPhysicalDevice physicalDevice, VkExten
     pdesc.depthTest = true;
     pdesc.blending = false;
 
-    // PIPELINE
     m_pipeline.Create(device, pdesc);
 
     std::cout << "[Vulkan] Scene-render pass created" << std::endl;
@@ -145,21 +32,9 @@ void ScenePass::Create(VkDevice device, VkPhysicalDevice physicalDevice, VkExten
 void ScenePass::Destroy(VkDevice device) {
 
     m_pipeline.Destroy(device);
-
-    if (m_framebuffer != VK_NULL_HANDLE) {
-        vkDestroyFramebuffer(device, m_framebuffer, nullptr);
-        m_framebuffer = VK_NULL_HANDLE;
-    }
-
     m_color.Destroy(device);
     m_depth.Destroy(device);
     m_resolve.Destroy(device);
-
-    if (m_renderPass != VK_NULL_HANDLE) {
-        vkDestroyRenderPass(device, m_renderPass, nullptr);
-        m_renderPass = VK_NULL_HANDLE;
-        std::cout << "[Vulkan] Scene-render pass destroyed" << std::endl;
-    }
 
 }
 
--- Engine/Graphics/Vulkan/ScenePass/ScenePass.h
+++ Engine/Graphics/Vulkan/ScenePass/ScenePass.h
@@ -26,17 +26,12 @@ public:
 
     VkExtent2D GetExtent() const { return m_renderExtent; }
 
-    [[nodiscard]] VkRenderPass Get() const { return m_renderPass; }
-
     RenderTarget& GetColor() { return m_color; }
     RenderTarget& GetDepth() { return m_depth; }
     RenderTarget& GetResolve() { return m_resolve; }
 
 
 private:
-
-    // PASS
-    VkRenderPass m_renderPass = VK_NULL_HANDLE;
 
     // PIPELINE
     Pipeline m_pipeline;
@@ -45,9 +40,6 @@ private:
     RenderTarget m_color;
     RenderTarget m_depth;
     RenderTarget m_resolve;
-
-    // FRAMEBUFFER
-    VkFramebuffer m_framebuffer = VK_NULL_HANDLE;
 
     VkExtent2D m_renderExtent = {};
 
